d4a8164fe54dd864cddabe818151f2d7fa05c1ec
OwenW24 <owenweis@tamu.edu>
2025-01-03 08:16:40 -0600

    small update before minimax

--- tttMM.py
+++ tttMM.py
@@ -38,8 +38,8 @@ def draw_figures(color=WHITE):
             if board[row][col] == 1:
                 pygame.draw.circle(screen, color, (int(col*SQUARE_SIZE + (SQUARE_SIZE // 2 ) ) , int(row*SQUARE_SIZE + (SQUARE_SIZE // 2 ) ) ), CIRCLE_RADIUS, CIRCLE_WIDTH)
             elif board[row][col] == 2: 
-                pygame.draw.line(screen, color, (col* SQUARE_SIZE + SQUARE_SIZE // 4 , row * SQUARE_SIZE + SQUARE_SIZE // 4), (col * SQUARE_SIZE + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4))
-                pygame.draw.line(screen, color, (col* SQUARE_SIZE + SQUARE_SIZE // 4 , row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4), (col * SQUARE_SIZE + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + SQUARE_SIZE // 4))
+                pygame.draw.line(screen, color, (col* SQUARE_SIZE + SQUARE_SIZE // 4 , row * SQUARE_SIZE + SQUARE_SIZE // 4), (col * SQUARE_SIZE + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4), CROSS_WIDTH)
+                pygame.draw.line(screen, color, (col* SQUARE_SIZE + SQUARE_SIZE // 4 , row * SQUARE_SIZE + 3 * SQUARE_SIZE // 4), (col * SQUARE_SIZE + 3 * SQUARE_SIZE // 4, row * SQUARE_SIZE + SQUARE_SIZE // 4), CROSS_WIDTH)
 
 def mark_square(row, col, player):
     board[row][col] = player
