b35b2ae88cc2d79248df11fae366a0e8cac1685a
OwenW24 <owenweis@tamu.edu>
2025-01-03 10:40:42 -0600

    minimax works

--- tttMM.py
+++ tttMM.py
@@ -69,7 +69,62 @@ def check_win(player, check_board = board):
     if check_board[2][0] == player and check_board[1][1] == player and check_board[0][2] == player:
         return True
     
+    return False
     
+    
+
+            
+            
+def minimax(minimax_board, depth, is_maximizing):
+    if check_win(2, minimax_board):
+        return float('inf')
+    elif check_win(1, minimax_board):
+        return float('-inf')
+    elif is_board_full(minimax_board):
+        return 0
+    
+    if is_maximizing:
+        best_score = float('-inf')
+        for row in range(BOARD_ROWS):
+            for col in range(BOARD_COLS):
+                if minimax_board[row][col] == 0:
+                    minimax_board[row][col] = 2
+                    score = minimax(minimax_board, depth + 1, False)
+                    minimax_board[row][col] = 0
+                    best_score = max(score, best_score)
+        return best_score
+                
+    else:
+        best_score = float('inf')
+        for row in range(BOARD_ROWS):
+            for col in range(BOARD_COLS):
+                if minimax_board[row][col] == 0:
+                    minimax_board[row][col] = 1
+                    score = minimax(minimax_board, depth + 1, True)
+                    minimax_board[row][col] = 0
+                    best_score = min(score, best_score)
+        return best_score
+    
+def best_move():
+    best_score = float('-inf')
+    move = (-1, -1)
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 0:
+                board[row][col] = 2
+                score = minimax(board, 0, False)
+                board[row][col] = 0
+                if score > best_score:
+                    best_score = score
+                    move = (row, col)
+                    
+    if move != (-1, -1):
+        mark_square(move[0], move[1], 2)
+        return True
+    return False
+
+
+
 def restart_game():
     screen.fill(BLACK)
     draw_lines()
@@ -78,10 +133,12 @@ def restart_game():
             board[row][col] = 0
             
             
+            
+             
+            
 
 def main():
     draw_lines()
-    print("Lines aint drawing")
     
     player = 1
     
@@ -103,6 +160,15 @@ def main():
                         game_over = True
                         
                     player = player % 2 + 1 # 1 to 2 and 2 to 1
+                    
+                    # minimax move
+                    if not game_over:
+                        if best_move():
+                            if check_win(2):
+                                game_over = True
+                            player = player % 2 + 1
+                    
+                    
                     
                     if not game_over:
                         if is_board_full():
